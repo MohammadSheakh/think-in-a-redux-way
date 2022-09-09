ba3ee26793115c00fec2f1cfcaf77d1c830285d4
MohammadSheakh <mohammad.sheakh@gmail.com>
2022-09-10 00:51:14 +0600

    ,

--- src/App.js
+++ src/App.js
@@ -4,7 +4,12 @@ import store from "./redux/store";
 
 export default function App() {
     return (
+        // Provider component diye wrap kore dilam // store nam er ekta prop ase ..
+        // shekhane bole dite hobe store ta ki ...store tao import kore niye ashte hobe
         <Provider store={store}>
+            {/* context API er maddhome store ta full application e diye dicche  
+                next kaj action dispatch kora ... 
+            */}
             <div className="w-screen h-screen p-10 bg-gray-100 text-slate-700">
                 <h1 className="max-w-md mx-auto text-center text-2xl font-bold">
                     Simple Counter Application
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,21 @@
+// redux folder er root e amra store ta create korlam...
+// karon store.js file amra amar full application e common
+// counter feature er moddhe sheta pore na .. onnano shob reducer o kintu ei store
+// er moddhei ashbe
+// amar full application er store kintu ektai thakbe
 import { createStore } from "redux";
 import counterReducer from "./counter/counterReducer";
 
-const store = createStore(counterReducer);
+const store = createStore(counterReducer); // store er moddhe amake reducer function
+// ta pass korte hoy
 
-export default store;
+export default store; // store ta keo export kore dilam
+
+/**
+ * amar shob kichui ready .. ekhon shudhu amar react application ke ei store ta
+ * provide korte hobe >>> Provider >>>
+ */
+/**
+ * root app.js ke somehow provide kore dite hobe redux store ta .. provider ta
+ * react-redux package amader ke diye diyeche
+ */
