52dc5922ce5287c92582022cd1c2b01383ae2df6
MohammadSheakh <mohammad.sheakh@gmail.com>
2022-09-10 21:06:13 +0600

    .

--- src/components/HooksCounter.js
+++ src/components/HooksCounter.js
@@ -1,8 +1,15 @@
 import { useSelector } from "react-redux";
 
 function HooksCounter({ id }) {
+    // selecting er beparta dekhlam .. shamne dispatching er bepar ta dekhbo
+
+    // useSelector .. she state theke kono kichu select kore dey
+    // shey chay .. vai amake ekta function deo .. tomar function use kore .. ami ashole tomake ..
+    // select kore dibo tomar state theke .. amra oke state dilam .. she amader ke state er value ta
+    // return kore dilo ..
+
     const count = useSelector((state) => state.value);
-    
+
     return (
         <div className="p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-white rounded shadow">
             <div className="text-2xl font-semibold">
