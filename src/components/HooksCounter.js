fdec0c647e2fce60a957874dcafcc4f449a87722
MohammadSheakh <mohammad.sheakh@gmail.com>
2022-09-10 19:29:30 +0600

    .

--- src/components/HooksCounter.js
+++ src/components/HooksCounter.js
@@ -2,7 +2,7 @@ import { useSelector } from "react-redux";
 
 function HooksCounter({ id }) {
     const count = useSelector((state) => state.value);
-
+    // boom
     return (
         <div className="p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-white rounded shadow">
             <div className="text-2xl font-semibold">
