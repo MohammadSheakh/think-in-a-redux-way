15d6c4205ba8666ecd649f9aa324215199e64b4f
MohammadSheakh <mohammad.sheakh@gmail.com>
2022-09-17 21:49:02 +0600

    ,

--- index.js
+++ index.js
@@ -4,29 +4,48 @@ const {
     fetchTodosMiddleware,
 } = require("./middlewares");
 
+// import js ekhane kaj korbe na .. amader ke common js .. require syntax e niye ashte hobe
+
 // initial state
 const initialState = {
+    // simple ekta object .. tar moddhe todos er ekta array thakbe
     todos: [],
 };
 
 // reducer
 const todoReducer = (state = initialState, action) => {
+    // amra jani reducer state ar action ney
     switch (action.type) {
         case "todos/todoAdded":
+            // definately amar action.payload er moddhe kichu ekta thakbe ..she onujayi
+            // state ta ke amar update kore dite hobe
             return {
+                // new object return korchi ... ja ase .. sheta ke age copy kore niye ashsi
                 ...state,
                 todos: [
+                    // tar por todos er moddhe .. jehetu eta ekta array .. amake abar
+                    // eta keo evabe copy kore niye ashte hobe
                     ...state.todos,
+                    // amar new jei jinish ta .. ami array of object chinta korchi ..
                     {
+                        // shekhan e amar title nam e ekta property ase .. shekhan e payload ta diye dilam
                         title: action.payload,
                     },
                 ],
             };
 
         case "todos/todoLoaded":
+            // kono ekta fake server e ekta request pathabo .. shei server  theke amra
+            // kichu todo pabo ..shei todo gula ultimately amar todos er moddhe insert kore dibo
             return {
                 ...state,
-                todos: [...state.todos, ...action.payload],
+                todos: [
+                    // eta jehetu ekta array .. so, er moddhe amar already jeta ase ..
+                    // sheta ke copy kore niye ashte hobe .. action.payload er moddhe amar
+                    // jei new todos gula ashbe .. sheta keo ami spread kore  nilam
+                    ...state.todos,
+                    ...action.payload,
+                ],
             };
 
         default:
@@ -34,13 +53,13 @@ const todoReducer = (state = initialState, action) => {
     }
 };
 
-// store
+// store // store er moddhe amader ke reducer bole dite hoy .. and middleware o bole deowa jay
 const store = createStore(
     todoReducer,
     applyMiddleware(delayActionMiddleware, fetchTodosMiddleware)
 );
 
-// subscribe to state changes
+// subscribe to state changes // reactive environment na thakle subscribe ta manually korte hobe
 store.subscribe(() => {
     console.log(store.getState());
 });
