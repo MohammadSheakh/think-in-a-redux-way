0275236cebedebd0254671f13f1ed6a8c1f3911c
MohammadSheakh <mohammad.sheakh@gmail.com>
2022-09-22 16:13:19 +0600

    .

--- redux/immerExample/index.js
+++ redux/immerExample/index.js
@@ -1,5 +1,5 @@
 const { createStore } = require("redux");
-const { produce } = require("immer");
+const { produce } = require("immer"); //
 
 const initialState = {
     name: "Learn with Sumit",
@@ -25,12 +25,16 @@ const reducer = (state = initialState, action) => {
             // return {
             //     ...state,
             //     address: {
+            // address property ta jehetu nije ekta object .. eta kintu immutably hobe na
             //         ...state.address,
-            //         street: action.payload,
+            //         street: action.payload, // jeta change hobe .. sheta ke change kore dilam
             //     },
             // };
             return produce(state, (draftState) => {
-                draftState.address.street = action.payload;
+                // produce function er call return kore dibo // 2nd parameter e she ekta callback expect
+                // kore //  vai ami tomake shora shori state dilam na .. tumi draft state ta iccha moto
+                // muted koro...
+                draftState.address.street = action.payload; // shora shori muted kore dilam
             });
 
         default:
