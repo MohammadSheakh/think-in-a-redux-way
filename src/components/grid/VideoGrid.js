7dd12c4fce1cbe8f58fdfd13aa2f5815fc218547
MohammadSheakh <mohammad.sheakh@gmail.com>
2022-09-25 00:10:46 +0600

    .

--- src/components/grid/VideoGrid.js
+++ src/components/grid/VideoGrid.js
@@ -5,13 +5,34 @@ import Loading from "../ui/Loading";
 import VideoGridItem from "./VideoGridItem";
 
 export default function VideGrid() {
+    /**
+     * VideoGrid er moddhe amra request ta pathabo ..request pathanor jonno amader ke
+     * action dispatch korte hobe.. amra ki dispatch korbo .. amra amader async thunk jeta
+     * baniyechilam ..fetchVideos() .. sheta ke amra dispatch  korbo ..
+     * dispatch korar jonno amader useDispatch ta lage ..
+     */
     const dispatch = useDispatch();
+    /**
+     * amake state ta ke read korte hobe somehow
+     */
     const { videos, isLoading, isError, error } = useSelector(
-        (state) => state.videos
+        // she amake full state ta diye dey .. tar vitor theke amake videos slice ta ber kore nite hobe
+        // and videos slice er moddhe initial state e jegula chilo.. shegula destructure kore nilam
+        (state) => state.videos // state. ei tuku full global state .. shetar videos node er moddhe
+        //initial state ta return kore .. destructure kore nite hobe
     );
+    /**
+     * request pathano .. ba ei dhoroner kaj gula ki kaj .. eta hocche side effect ..
+     * ei dhoroner kaj useEffect hook er moddhe korte hoy ...
+     */
 
+    // component render houar pore useEffect ekbar kaj korbe // erpor request ta jabe
     useEffect(() => {
-        dispatch(fetchVideos());
+        // ekta callback function dite hoy
+        dispatch(fetchVideos()); // async thunk function dispatch korte hobe ..
+        // eta ke ekhane call korte hobe .. karon eita async thunk .. eta action creator
+        // er moto kaj kore .. so, action creator ke call korle .. amra actual action
+        // ta pai
     }, [dispatch]);
 
     // decide what to render
