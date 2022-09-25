54c5b9fa830ef820574aff82d4a90f84db42a875
MohammadSheakh <mohammad.sheakh@gmail.com>
2022-09-25 12:06:14 +0600

    .

--- src/components/tags/Tag.js
+++ src/components/tags/Tag.js
@@ -1,5 +1,6 @@
 import { useDispatch, useSelector } from "react-redux";
 import { tagRemoved, tagSelected } from "../../features/filter/filterSlice";
+// action gula niye ashlam .. 
 /**
  * ekhane select korar o feature rakhte hobe 
  * mane etar onClick e kichu ekta korte hobe ..  
@@ -9,7 +10,9 @@ export default function Tag({ title }) {
     const dispatch = useDispatch();
     const { tags: selectedTags } = useSelector((state) => state.filter);
     // tags: selectedTags ... bojhar jonno alias kore dilam 
-    const isSelected = selectedTags.includes(title) ? true : false;
+    const isSelected = selectedTags.includes(title) ? true : false; // selectedTags er moddhe 
+    // title ta thakle .. sheta selected .. // current tag ta selected ase kina .. sheta 
+    // jante parbo .. 
 
     const style = isSelected
         ? "bg-blue-600 text-white px-4 py-1 rounded-full cursor-pointer"
@@ -17,9 +20,9 @@ export default function Tag({ title }) {
 
     const handleSelect = () => {
         if (isSelected) {
-            dispatch(tagRemoved(title));
+            dispatch(tagRemoved(title)); // selected thakle tag removesd action dispatch korbo .. 
         } else {
-            dispatch(tagSelected(title));
+            dispatch(tagSelected(title));// action.payload er moddhe expect kortese .. 
         }
     };
 
