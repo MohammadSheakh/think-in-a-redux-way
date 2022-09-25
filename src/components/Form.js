5dcd20940991243bc6576abfd90ff2c9a21188e8
MohammadSheakh <mohammad.sheakh@gmail.com>
2022-09-25 20:38:07 +0600

    transaction

--- src/components/Form.js
+++ src/components/Form.js
@@ -6,34 +6,35 @@ import {
 } from "../features/transaction/transactionSlice";
 
 export default function Form() {
-    // form er data amake locally handle korte hobe .. state manage korte hobe .. 
-    // useState use korbo amra ejonno .. 
+    // form er data amake locally handle korte hobe .. state manage korte hobe ..
+    // useState use korbo amra ejonno ..
     const [name, setName] = useState("");
     const [type, setType] = useState("");
     const [amount, setAmount] = useState("");
-    const [editMode, setEditMode] = useState(false); // edit korbo ki korbo na ... 
-    // eta select korar jonno amra ekta state nicchi ... 
+    const [editMode, setEditMode] = useState(false); // edit korbo ki korbo na ...
+    // eta select korar jonno amra ekta state nicchi ...
 
     const dispatch = useDispatch();
     const { isLoading, isError } = useSelector((state) => state.transaction);
     const { editing } = useSelector((state) => state.transaction) || {};
 
+    // edit button e click korlei render hobe ...
     // listen for edit mode active
     useEffect(() => {
         const { id, name, amount, type } = editing || {};
         if (id) {
-            // id thakle amra edit mode true korbo .. otherwise false korbo .. 
-            setEditMode(true);
+            // id thakle amra edit mode true korbo .. otherwise false korbo ..
+            setEditMode(true); // jeno cancel edit mode button ta chole ashe
             setName(name);
             setType(type);
             setAmount(amount);
         } else {
-            setEditMode(false);
+            setEditMode(false); // jeno cancel edit mode button ta hide hoye jay
             reset();
         }
-    }, [editing]);
+    }, [editing]); // editing jokhon change hobe .. tokhon e re render hobe
 
-    // reset mane form er value "" hoye jaowa ... 
+    // reset mane form er value "" hoye jaowa ...
     const reset = () => {
         setName("");
         setType("");
@@ -41,11 +42,11 @@ export default function Form() {
     };
 
     const handleCreate = (e) => {
-        // form submit korle .. ei function kaj korbe .. 
-        e.preventDefault();// page jeno reload na ney .. ejonno ei ta .. 
-        // amader ke ekta thunk function dispatch korte hoy ... er moddhe ekta object 
-        // pass kora jay .. jetar modde ami name, type, amount bole dite pari .. 
-        // amount ta Number() er moddhe pass korte hobe .. jeno calculation kora jay 
+        // form submit korle .. ei function kaj korbe ..
+        e.preventDefault(); // page jeno reload na ney .. ejonno ei ta ..
+        // amader ke ekta thunk function dispatch korte hoy ... er moddhe ekta object
+        // pass kora jay .. jetar modde ami name, type, amount bole dite pari ..
+        // amount ta Number() er moddhe pass korte hobe .. jeno calculation kora jay
         dispatch(
             createTransaction({
                 name,
@@ -53,19 +54,19 @@ export default function Form() {
                 amount: Number(amount),
             })
         );
-        reset(); // form er data gula reset kore dite hobe .. 
+        reset(); // form er data gula reset kore dite hobe ..
     };
 
     const handleUpdate = (e) => {
-        //update button e click korle ekta event fire hoy ... 
-        e.preventDefault(); // jeno page reload na ney 
-        // amra ekta thunk function dispatch kori .. 
+        //update button e click korle ekta event fire hoy ...
+        e.preventDefault(); // jeno page reload na ney
+        // amra ekta thunk function dispatch kori ..
         dispatch(
-            // jehtu update er bepar ... tai amader ekta id lagbe .. kon transaction 
-            // update korbo sheta track rakhar jonno .. 
+            // jehtu update er bepar ... tai amader ekta id lagbe .. kon transaction
+            // update korbo sheta track rakhar jonno ..
             changeTransaction({
-                // ekhaneo amader ke ekta object bole dite hoy ... er moddhe amra 
-                // value pass korbo .. 
+                // ekhaneo amader ke ekta object bole dite hoy ... er moddhe amra
+                // value pass korbo ..
                 id: editing?.id,
                 data: {
                     name: name,
@@ -74,11 +75,11 @@ export default function Form() {
                 },
             })
         );
-        setEditMode(false); // edit kora hoye gele .. setEditMode() false kore dite hobe .. 
+        setEditMode(false); // edit kora hoye gele .. setEditMode() false kore dite hobe ..
         reset(); // form o reset kore dite hobe ..  .
     };
 
-    // edit mode cancel korte chaile  . 
+    // edit mode cancel korte chaile  .
     const cancelEditMode = () => {
         reset();
         setEditMode(false);
@@ -94,7 +95,7 @@ export default function Form() {
                 <div className="form-group">
                     <label>Name</label>
                     <input
-                    // name ar value er value same dite hobe 
+                        // name ar value er value same dite hobe
                         type="text"
                         name="name"
                         required
@@ -111,10 +112,10 @@ export default function Form() {
                             required
                             type="radio"
                             value="income"
-                            name="type" // state.type  // duita radio element er e same name dite hoy 
-                            checked={type === "income"} // jei jinish ta selected hoye ase shei jinish ta 
+                            name="type" // state.type  // duita radio element er e same name dite hoy
+                            checked={type === "income"} // jei jinish ta selected hoye ase shei jinish ta
                             // check true hobe jodi ei khetre amar type income  hoy
-                            onChange={(e) => setType("income")} // check hole income hobe ... 
+                            onChange={(e) => setType("income")} // check hole income hobe ...
                         />
                         <label>Income</label>
                     </div>
@@ -122,10 +123,10 @@ export default function Form() {
                         <input
                             type="radio"
                             value="expense"
-                            name="type" // state.type 
+                            name="type" // state.type
                             placeholder="Expense"
                             // check true hobe jodi ei khetre amar type expense hoy
-                            checked={type === "expense"}// jei jinish ta selected hoye ase shei jinish ta 
+                            checked={type === "expense"} // jei jinish ta selected hoye ase shei jinish ta
                             onChange={(e) => setType("expense")}
                         />
                         <label>Expense</label>
@@ -136,7 +137,7 @@ export default function Form() {
                     <label>Amount</label>
                     <input
                         type="number"
-                        required // basic higine ... 
+                        required // basic higine ...
                         placeholder="enter amount"
                         name="amount"
                         value={amount}
@@ -150,9 +151,10 @@ export default function Form() {
                     {editMode ? "Update Transaction" : "Add Transaction"}
                 </button>
 
-                {!isLoading && isError && ( // error thakle error show korbo ... 
-                    <p className="error">There was an error occured</p>
-                )}
+                {!isLoading &&
+                    isError && ( // error thakle error show korbo ...
+                        <p className="error">There was an error occured</p>
+                    )}
             </form>
 
             {/* edit mode on thaklei .. edit mode cancel korar jonno button dekhte parbo  */}
--- src/features/transaction/transactionSlice.js
+++ src/features/transaction/transactionSlice.js
@@ -11,19 +11,19 @@ const initialState = {
     isLoading: false,
     isError: false,
     error: "",
-    editing: {},
+    editing: {}, // jei information ta amra edit korte chai .. sheta ke ekhane rekhe dibo
 };
 
 // ek ekta API handle korar jonno ek ekta thunk function
-// dispatch korar shomoy ei thunk function gula UI te 
+// dispatch korar shomoy ei thunk function gula UI te
 // lagbe .. tai amra export kore dibo ekhan thekei
-// egulai amar ek ekta action creator ... 
-// convention hocche folder er nam diye shuru korte hoy 
-// er pore async function tar nam dite hoy ... 
+// egulai amar ek ekta action creator ...
+// convention hocche folder er nam diye shuru korte hoy
+// er pore async function tar nam dite hoy ...
 // er pore toolkit automatically / dynamically
-// reducer function er nam er upor base kore 
-// action er nam thik kore dey .. logger e amra sheta 
-// dekhte pari ..  
+// reducer function er nam er upor base kore
+// action er nam thik kore dey .. logger e amra sheta
+// dekhte pari ..
 // async thunks
 export const fetchTransactions = createAsyncThunk(
     "transaction/fetchTransactions",
@@ -33,9 +33,9 @@ export const fetchTransactions = createAsyncThunk(
     }
 );
 
-// ei thunk function ta ke jokhon dispatch korbo .. 
-// tokhon UI theke data diye dibo ... karon 
-// addTransaction API data expect kore .. 
+// ei thunk function ta ke jokhon dispatch korbo ..
+// tokhon UI theke data diye dibo ... karon
+// addTransaction API data expect kore ..
 export const createTransaction = createAsyncThunk(
     "transaction/createTransaction",
     async (data) => {
@@ -46,10 +46,10 @@ export const createTransaction = createAsyncThunk(
 
 // object akare keno
 // amra jani jokhon amra action dispatch korbo .. tokhon
-// parameter e kintu ektai parameter pathate pari .. 
-// tai na .. jar karone amake object akare pathate hobe .. 
+// parameter e kintu ektai parameter pathate pari ..
+// tai na .. jar karone amake object akare pathate hobe ..
 // tar id property er modde id diye dibo .. data property
-// er moddhe data diye dibo .. 
+// er moddhe data diye dibo ..
 export const changeTransaction = createAsyncThunk(
     "transaction/changeTransaction",
     async ({ id, data }) => {
@@ -66,15 +66,16 @@ export const removeTransaction = createAsyncThunk(
     }
 );
 
-// thunk gula banano shesh .. ebar ami amar slice ta 
-// banate pari .. 
+// thunk gula banano shesh .. ebar ami amar slice ta
+// banate pari ..
 // create slice
 const transactionSlice = createSlice({
     name: "transaction", // nam dite hoy
     initialState,
     reducers: {
+        // edit korte chaile editing {} er moddhe data assign hoye jabe ..
         editActive: (state, action) => {
-            state.editing = action.payload;
+            state.editing = action.payload; // full transaction er payload tai diye dibo
         },
         editInActive: (state) => {
             state.editing = {};
@@ -85,7 +86,7 @@ const transactionSlice = createSlice({
     extraReducers: (builder) => {
         builder
             .addCase(fetchTransactions.pending, (state) => {
-                // eita hocche amar reducer function ... 
+                // eita hocche amar reducer function ...
                 state.isError = false;
                 state.isLoading = true;
             })
@@ -107,38 +108,38 @@ const transactionSlice = createSlice({
             .addCase(createTransaction.fulfilled, (state, action) => {
                 state.isError = false;
                 state.isLoading = false;
-                state.transactions.push(action.payload); // shora shori muted 
-                // korte pari amra ... 
+                state.transactions.push(action.payload); // shora shori muted
+                // korte pari amra ...
             })
             .addCase(createTransaction.rejected, (state, action) => {
                 state.isLoading = false;
                 state.isError = true;
                 state.error = action.error?.message;
-                // state.transactions = []; ei ta bola lagbe na ... 
+                // state.transactions = []; ei ta bola lagbe na ...
             })
             .addCase(changeTransaction.pending, (state) => {
                 state.isError = false;
                 state.isLoading = true;
             })
             .addCase(changeTransaction.fulfilled, (state, action) => {
-                // edit  jodi fulfilled hoy ... 
+                // edit  jodi fulfilled hoy ...
                 state.isError = false;
                 state.isLoading = false;
                 /**
-                 * particular jinish ta ke khuje ber kore .. oi jinish ta ke update 
-                 * kore dite hobe ... server e ekta jinish change hoyeche .. change 
+                 * particular jinish ta ke khuje ber kore .. oi jinish ta ke update
+                 * kore dite hobe ... server e ekta jinish change hoyeche .. change
                  * houar pore .. ami kintu updated jinis ta peyechi..and amar action
-                 * jeta ase .. shei action er moddhe kintu amar .. action.payload 
-                 * er moddhe kintu amar updated jinish ta diye dibe .. so, amar jeta 
-                 * kora lagbe .. oi id owala jinish ta amar local state e khuje ber 
-                 * kore , tar pore sheta ke update korte hobe .. 
+                 * jeta ase .. shei action er moddhe kintu amar .. action.payload
+                 * er moddhe kintu amar updated jinish ta diye dibe .. so, amar jeta
+                 * kora lagbe .. oi id owala jinish ta amar local state e khuje ber
+                 * kore , tar pore sheta ke update korte hobe ..
                  */
                 /**
-                 * index ta age khuje ber kori .. je .. kon id ta age amader ke 
-                 * khuje ber korte hobe .. 
-                 * prottek bar ekta kroe transaction pabo .. jei transaction er id 
-                 * er shathe action.payload er id mile jabe ... sheta ke update 
-                 * korbo 
+                 * index ta age khuje ber kori .. je .. kon id ta age amader ke
+                 * khuje ber korte hobe ..
+                 * prottek bar ekta kroe transaction pabo .. jei transaction er id
+                 * er shathe action.payload er id mile jabe ... sheta ke update
+                 * korbo
                  */
                 const indexToUpdate = state.transactions.findIndex(
                     (t) => t.id === action.payload.id
@@ -159,12 +160,12 @@ const transactionSlice = createSlice({
                 console.log(action);
                 state.isError = false;
                 state.isLoading = false;
-                // delete er khetre amader ke filter method use korte hoy .. 
+                // delete er khetre amader ke filter method use korte hoy ..
                 state.transactions = state.transactions.filter(
                     (t) => t.id !== action.meta.arg
-                    // jei jei jinish gula milbe na .. shegula ke rakhbo .. 
+                    // jei jei jinish gula milbe na .. shegula ke rakhbo ..
                 );
-                // muted kore felechi .. so, amar delete hoye jaowar kotha .. 
+                // muted kore felechi .. so, amar delete hoye jaowar kotha ..
             })
             .addCase(removeTransaction.rejected, (state, action) => {
                 state.isLoading = false;
@@ -174,5 +175,5 @@ const transactionSlice = createSlice({
     },
 });
 
-export default transactionSlice.reducer; // reducer ta ke default export kore dite hobe 
+export default transactionSlice.reducer; // reducer ta ke default export kore dite hobe
 export const { editActive, editInActive } = transactionSlice.actions;
