d906e03e4812a5916c36322cab55306c4bcf35e8
MohammadSheakh <mohammad.sheakh@gmail.com>
2022-09-25 11:21:24 +0600

    .

--- src/components/tags/Tag.js
+++ src/components/tags/Tag.js
@@ -1,10 +1,14 @@
 import { useDispatch, useSelector } from "react-redux";
 import { tagRemoved, tagSelected } from "../../features/filter/filterSlice";
-
+/**
+ * ekhane select korar o feature rakhte hobe 
+ * mane etar onClick e kichu ekta korte hobe ..  
+ * 
+ */
 export default function Tag({ title }) {
     const dispatch = useDispatch();
     const { tags: selectedTags } = useSelector((state) => state.filter);
-
+    // tags: selectedTags ... bojhar jonno alias kore dilam 
     const isSelected = selectedTags.includes(title) ? true : false;
 
     const style = isSelected
--- src/features/filter/filterSlice.js
+++ src/features/filter/filterSlice.js
@@ -1,5 +1,12 @@
 const { createSlice } = require("@reduxjs/toolkit");
-
+/**
+ * ekhane amra shudhu selection korbo .. amra ekhane
+ * kono API request korbo na .. 
+ * amra local reducer niyei kaj korbo .. jeta amader 
+ * old jei kaj gula amra korechi .. async kaj gula 
+ * amra korbo na .. tai async thunk er o proyojon 
+ * hobe na .. 
+ */
 const initialState = {
     tags: [],
     search: "",
@@ -10,13 +17,22 @@ const filterSlice = createSlice({
     initialState,
     reducers: {
         tagSelected: (state, action) => {
+            // ekhane amake reducer function ta likhte hobe .. 
             state.tags.push(action.payload);
+            // state er tags er array te push korte hobe 
+            // ekhane shora shori state ke muted korte parbo 
         },
         tagRemoved: (state, action) => {
+            // tags array te jodi oi tag ta theke thake
+            // taile shei tag ta remove korbo 
             const indexToRemove = state.tags.indexOf(action.payload);
+            // age dekhi je array te index ta ase kina ... 
+            // shei index ta age ber kori .. tarpore .. 
 
             if (indexToRemove !== -1) {
                 state.tags.splice(indexToRemove, 1);
+                // splice use kore amra remove kore dite parbo
+                // kon index theke remove korbo , koyta element remove korbo .. 
             }
         },
         searched: (state, action) => {
@@ -27,3 +43,4 @@ const filterSlice = createSlice({
 
 export default filterSlice.reducer;
 export const { tagSelected, tagRemoved, searched } = filterSlice.actions;
+// action gulao dispatch korte hobe ... 
